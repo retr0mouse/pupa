791eb16606b4cdc6662238f41ea26ef630675450
retr0mouse <daniil.sharin667@gmail.com>
2022-03-07 22:51:20 +0200

    add input on client side

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default function App() {
+    const [quizzes, setQuizzes] = useState([""]);
+    const tempQuizzes: string[] = ["one", "two", "three"];
+    const [query, setQuery] = useState("");
+
     return (
         <div>
-            <p>Hello world!</p>
+            <input type="text" placeholder="name goes here" value={query} onChange={(event) => setQuery(event.target.value)}></input>
+            <button onClick={() => createQuiz(query)}>Create quiz</button>
+            <ul>
+            {quizzes.map((quiz) => {
+                return (
+                    <li>{quiz}</li>
+                )
+            })}
+            </ul>
+            {console.log(quizzes)}
         </div>
     );
+
+    function createQuiz(name: string) {
+        let newArr = [quizzes.keys, name];
+        console.log(newArr);
+        setQuizzes([name]);
+    }
 }
