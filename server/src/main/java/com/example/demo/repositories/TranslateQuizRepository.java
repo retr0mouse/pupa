830d072dd6407e05222b35ff2da706e52f7de4a6
retr0mouse <daniil.sharin667@gmail.com>
2022-06-10 13:49:58 +0300

    add translate quiz table to database(not finished)
    Co-authored-by: Daniil_Stoljar <Danistol@users.noreply.github.com>

---
+++ server/src/main/java/com/example/demo/repositories/TranslateQuizRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.TranslateQuiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TranslateQuizRepository extends JpaRepository<TranslateQuiz, Long> {
+}
