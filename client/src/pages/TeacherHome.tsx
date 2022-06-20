c6511a96a11ac266287783c3026910d6ca157c5a
Daniil <daniil.sharin667@gmail.com>
2022-06-20 13:55:52 +0300

    add user id on teacher home page

--- client/src/pages/TeacherHome.tsx
+++ client/src/pages/TeacherHome.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { UserAPI } from "../apis/UserAPI";
 import { Navigation } from "../components/NavigationBar";
 
 const Container = styled.div`
@@ -40,17 +41,30 @@ const Container = styled.div`
         font-family: 'Open Sans', sans-serif;
         font-size: 44px;
     }
+
+    h2 {
+        margin-top: 0;
+        font-family: 'Poppins', sans-serif;
+        font-size: 36px;
+    }
 `;
 
 export function TeacherHome() {
+    const [userId, setUserId] = useState(0);
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
     return (<>
-    <Navigation
-        link="/teacher"
-        enableRolesButton={true}
-        enableLogoutButton={true}
-    />
+        <Navigation
+            link="/teacher"
+            enableRolesButton={true}
+            enableLogoutButton={true}
+        />
         <Container>
             <h1>Teacher Home Page</h1>
+            <h2>User id: {userId}</h2>
             <NavLink to="/create-pack">
                 <button>
                     Create new pack
@@ -64,4 +78,13 @@ export function TeacherHome() {
         </Container>
     </>
     );
+
+    async function getUser() {
+        try {
+            const user = await UserAPI.GetUser();
+            setUserId(user.id);
+        } catch(error) {
+            throw new Error("error" + error);
+        }
+    }
 } 
